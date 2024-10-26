724710320078773ed6b581ef967ffebd409e88d3
DevMatch User <someone@devmatch.io>
2024-10-26 11:40:05 -0700

    Initial commit; Goal 1

--- dogApi.js
+++ dogApi.js
@@ -1,3 +1,16 @@
 // dogApi.js
 
 // Add your code here
+export async function fetchBreeds() {
+    const url = await fetch("https://dogapi.dog/api/v2/breeds");
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        const json = await response.json();
+        console.log(json);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
